e6217f4463ab9435a6a934a2c0a0510aa0326be5
amichai07 <amichai.horvitz@bluevine.com>
2019-07-10 20:36:03 +0300

    collected trigger rule dep check per dag run

--- airflow/ti_deps/deps/trigger_rule_dep.py
+++ airflow/ti_deps/deps/trigger_rule_dep.py
@@ -76,21 +76,21 @@ class TriggerRuleDep(BaseTIDep):
             qry = (
                 session
                 .query(
-                        func.coalesce(func.sum(
-                            case([(TI.state == State.SUCCESS, 1)], else_=0)), 0),
-                        func.coalesce(func.sum(
-                            case([(TI.state == State.SKIPPED, 1)], else_=0)), 0),
-                        func.coalesce(func.sum(
-                            case([(TI.state == State.FAILED, 1)], else_=0)), 0),
-                        func.coalesce(func.sum(
-                            case([(TI.state == State.UPSTREAM_FAILED, 1)], else_=0)), 0),
-                        func.count(TI.task_id),
+                    func.coalesce(func.sum(
+                        case([(TI.state == State.SUCCESS, 1)], else_=0)), 0),
+                    func.coalesce(func.sum(
+                        case([(TI.state == State.SKIPPED, 1)], else_=0)), 0),
+                    func.coalesce(func.sum(
+                        case([(TI.state == State.FAILED, 1)], else_=0)), 0),
+                    func.coalesce(func.sum(
+                        case([(TI.state == State.UPSTREAM_FAILED, 1)], else_=0)), 0),
+                    func.count(TI.task_id),
                 )
                 .filter(
-                        TI.dag_id == ti.dag_id,
-                        TI.task_id.in_(ti.task.upstream_task_ids),
-                        TI.execution_date == ti.execution_date,
-                        TI.state.in_(State.finished()),
+                    TI.dag_id == ti.dag_id,
+                    TI.task_id.in_(ti.task.upstream_task_ids),
+                    TI.execution_date == ti.execution_date,
+                    TI.state.in_(State.finished()),
                 )
             )
             successes, skipped, failed, upstream_failed, done = qry.first()
@@ -112,15 +112,15 @@ class TriggerRuleDep(BaseTIDep):
 
     @provide_session
     def _evaluate_trigger_rule(
-            self,
-            ti,
-            successes,
-            skipped,
-            failed,
-            upstream_failed,
-            done,
-            flag_upstream_failed,
-            session):
+        self,
+        ti,
+        successes,
+        skipped,
+        failed,
+        upstream_failed,
+        done,
+        flag_upstream_failed,
+        session):
         """
         Yields a dependency status that indicate whether the given task instance's trigger
         rule was met.
@@ -188,60 +188,60 @@ class TriggerRuleDep(BaseTIDep):
             if successes <= 0:
                 yield self._failing_status(
                     reason="Task's trigger rule '{0}' requires one upstream "
-                    "task success, but none were found. "
-                    "upstream_tasks_state={1}, upstream_task_ids={2}"
-                    .format(tr, upstream_tasks_state, task.upstream_task_ids))
+                           "task success, but none were found. "
+                           "upstream_tasks_state={1}, upstream_task_ids={2}"
+                        .format(tr, upstream_tasks_state, task.upstream_task_ids))
         elif tr == TR.ONE_FAILED:
             if not failed and not upstream_failed:
                 yield self._failing_status(
                     reason="Task's trigger rule '{0}' requires one upstream "
-                    "task failure, but none were found. "
-                    "upstream_tasks_state={1}, upstream_task_ids={2}"
-                    .format(tr, upstream_tasks_state, task.upstream_task_ids))
+                           "task failure, but none were found. "
+                           "upstream_tasks_state={1}, upstream_task_ids={2}"
+                        .format(tr, upstream_tasks_state, task.upstream_task_ids))
         elif tr == TR.ALL_SUCCESS:
             num_failures = upstream - successes
             if num_failures > 0:
                 yield self._failing_status(
                     reason="Task's trigger rule '{0}' requires all upstream "
-                    "tasks to have succeeded, but found {1} non-success(es). "
-                    "upstream_tasks_state={2}, upstream_task_ids={3}"
-                    .format(tr, num_failures, upstream_tasks_state,
-                            task.upstream_task_ids))
+                           "tasks to have succeeded, but found {1} non-success(es). "
+                           "upstream_tasks_state={2}, upstream_task_ids={3}"
+                        .format(tr, num_failures, upstream_tasks_state,
+                                task.upstream_task_ids))
         elif tr == TR.ALL_FAILED:
             num_successes = upstream - failed - upstream_failed
             if num_successes > 0:
                 yield self._failing_status(
                     reason="Task's trigger rule '{0}' requires all upstream "
-                    "tasks to have failed, but found {1} non-failure(s). "
-                    "upstream_tasks_state={2}, upstream_task_ids={3}"
-                    .format(tr, num_successes, upstream_tasks_state,
-                            task.upstream_task_ids))
+                           "tasks to have failed, but found {1} non-failure(s). "
+                           "upstream_tasks_state={2}, upstream_task_ids={3}"
+                        .format(tr, num_successes, upstream_tasks_state,
+                                task.upstream_task_ids))
         elif tr == TR.ALL_DONE:
             if not upstream_done:
                 yield self._failing_status(
                     reason="Task's trigger rule '{0}' requires all upstream "
-                    "tasks to have completed, but found {1} task(s) that "
-                    "weren't done. upstream_tasks_state={2}, "
-                    "upstream_task_ids={3}"
-                    .format(tr, upstream_done, upstream_tasks_state,
-                            task.upstream_task_ids))
+                           "tasks to have completed, but found {1} task(s) that "
+                           "weren't done. upstream_tasks_state={2}, "
+                           "upstream_task_ids={3}"
+                        .format(tr, upstream_done, upstream_tasks_state,
+                                task.upstream_task_ids))
         elif tr == TR.NONE_FAILED:
             num_failures = upstream - successes - skipped
             if num_failures > 0:
                 yield self._failing_status(
                     reason="Task's trigger rule '{0}' requires all upstream "
-                    "tasks to have succeeded or been skipped, but found {1} non-success(es). "
-                    "upstream_tasks_state={2}, upstream_task_ids={3}"
-                    .format(tr, num_failures, upstream_tasks_state,
-                            task.upstream_task_ids))
+                           "tasks to have succeeded or been skipped, but found {1} non-success(es). "
+                           "upstream_tasks_state={2}, upstream_task_ids={3}"
+                        .format(tr, num_failures, upstream_tasks_state,
+                                task.upstream_task_ids))
         elif tr == TR.NONE_SKIPPED:
             if skipped > 0:
                 yield self._failing_status(
                     reason="Task's trigger rule '{0}' requires all upstream "
-                    "tasks to not have been skipped, but found {1} task(s) skipped. "
-                    "upstream_tasks_state={2}, upstream_task_ids={3}"
-                    .format(tr, skipped, upstream_tasks_state,
-                            task.upstream_task_ids))
+                           "tasks to not have been skipped, but found {1} task(s) skipped. "
+                           "upstream_tasks_state={2}, upstream_task_ids={3}"
+                        .format(tr, skipped, upstream_tasks_state,
+                                task.upstream_task_ids))
         else:
             yield self._failing_status(
                 reason="No strategy to evaluate trigger rule '{0}'.".format(tr))
